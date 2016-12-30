585d26e565da2094060a503c3ba64d27be449bd7
Tomas Carnecky <tomas.carnecky@gmail.com>
2016-12-30 11:54:34 +0100

    link: higher-order element for creating router links
    The link element adds a onClick handler to the wrapped element and pushes
    the given href to the router stack when clicked.

--- pkg/hs/nauva/src/Nauva/Service/Router.hs
+++ pkg/hs/nauva/src/Nauva/Service/Router.hs
@@ -1,11 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Nauva.Service.Router
     ( RouterH(..)
     , Location(..)
+
+    , LinkProps(..)
+    , link
     ) where
 
 
-import Data.Text (Text)
-import Control.Concurrent.STM
+import           Data.Text  (Text)
+import qualified Data.Aeson as A
+
+import           Control.Concurrent.STM
+
+import           Nauva.Internal.Types
+import           Nauva.Internal.Events
+import           Nauva.View
+import           Nauva.NJS
 
 
 
@@ -23,3 +36,48 @@ data RouterH = RouterH
 data Location = Location
     { locPathname :: !Text
     }
+
+
+
+-------------------------------------------------------------------------------
+-- link
+
+data LinkProps = LinkProps
+    { routerH :: !RouterH
+      -- ^ The router handle provides the callbacks.
+    , href :: !Text
+      -- ^ The path which shall be pushed to the router history when the element
+      -- is clicked.
+    , element :: !Element
+      -- ^ The element to which a 'onClick' handler is attached.
+    }
+
+link :: LinkProps -> Element
+link = component_ linkComponent
+
+
+
+linkComponent :: Component LinkProps () LinkProps ()
+linkComponent = Component
+    { componentId = mkComponentId
+    , componentDisplayName = "Link"
+    , initialComponentState = \props -> pure (props, [])
+    , componentEventListeners = \_ -> []
+    , componentHooks = emptyHooks
+    , processLifecycleEvent = \() s -> (s, [])
+    , receiveProps = \props _ -> pure (props, [], [])
+    , update = \() s -> (s, [clickEffect s])
+    , renderComponent = \(LinkProps {..}) -> with element [onClick_ onClickHandler]
+    , componentSnapshot = \_ -> A.object []
+    , restoreComponent = \_ s -> Right (s, [])
+    }
+  where
+    onClickHandler :: F1 MouseEvent (EventHandler ())
+    onClickHandler = eventHandler $ \ev -> do
+        preventDefault
+        action $ value0E $ njsCon0 "()" ()
+
+    clickEffect :: LinkProps -> IO (Maybe ())
+    clickEffect (LinkProps {..}) = do
+        hPush routerH href
+        pure Nothing
