6d8bc6ee41985fc4431872d41a0ad57718cb1700
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-04-18 22:25:29 +0200

    Nauva.CSS.Typeface

--- pkg/hs/nauva-css/src/Nauva/CSS.hs
+++ pkg/hs/nauva-css/src/Nauva/CSS.hs
@@ -2,6 +2,7 @@ module Nauva.CSS
     ( module X
     ) where
 
-import Nauva.CSS.Helpers as X
-import Nauva.CSS.Terms   as X
-import Nauva.CSS.Types   as X
+import Nauva.CSS.Helpers  as X
+import Nauva.CSS.Terms    as X
+import Nauva.CSS.Typeface as X
+import Nauva.CSS.Types    as X
---
+++ pkg/hs/nauva-css/src/Nauva/CSS/Typeface.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Nauva.CSS.Typeface
+    ( Typeface(..)
+    , typeFace
+    ) where
+
+
+import Data.Text (Text)
+import Nauva.CSS.Terms
+import Nauva.CSS.Types
+import Control.Monad.Writer.Lazy
+
+
+
+-- | A 'Typeface' is a combination of font family, font weight, font size,
+-- line height and font variation. The four properties together identify
+-- a particular type face which is used in one or more places inside your
+-- application.
+--
+-- You should not define new type faces ad-hoc inside individual components.
+-- Instead, define them once in a single place and then reuse. This allows
+-- you to manage an index of all typefaces which are used throughout your
+-- application, show them all in the catalog, and prevents the number
+-- of distinct typefaces from exploding.
+--
+-- A 'Typeface' also has a name, which should be unique and is only used
+-- in the catalog, for documentation purposes.
+--
+-- Note: the font family is specified using a 'CSSValue'. This means custom
+-- @font-face is not supported at the moment (see 'fontFamily_' or 'DFontFamily').
+
+data Typeface = Typeface
+    { tfName :: Text
+    , tfFontFamily :: CSSValue
+    , tfFontWeight :: CSSValue
+    , tfFontSize :: CSSValue
+    , tfLineHeight :: CSSValue
+    }
+
+
+-- | Apply the given 'Typeface' in a CSS block.
+--
+-- > rootStyle = mkStyle $ do
+-- >     typeFace someTypeFace
+-- >     color "black"
+
+typeFace :: Typeface -> Writer [Statement] ()
+typeFace (Typeface {..}) = do
+    fontFamily tfFontFamily
+    fontWeight tfFontWeight
+    fontSize tfFontSize
+    lineHeight tfLineHeight
